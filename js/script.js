9c4973b9342618a71a3895b4decce14d6991e987
RdFaysal <faysalmiah4568@gmail.com>
2026-03-08 22:57:41 +0600

    header & tabs button added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+
 function login(){
 
 const username = document.getElementById("username").value
 const password = document.getElementById("password").value
 
 if(username === "admin" && password === "admin123"){
-    window.location.href = "main.html"
-}
-else{
-    alert("Invalid Login")
+
+window.location.href = "main.html"
+
+}else{
+
+alert("Invalid Login")
+
 }
 
 }
